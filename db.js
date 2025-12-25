67fd4509a4f9ec328a3c66b4f7953eb0b88253b0
Noman6542 <mdaanoman6@gmail.com>
2025-12-26 03:16:06 +0600

    added cors

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
-// db.js
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  user: 'root',
+  password: "",
+  database: 'admission_bridge'
 });
